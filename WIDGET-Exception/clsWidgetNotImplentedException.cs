bc3463ec37b2b48904caac50ec230a09f2ddeba5
steven-p-sutton <steven.p.sutton@gmail.com>
2020-11-26 10:14:22 +0000

    update

---
+++ WIDGET-Exception/clsWidgetNotImplentedException.cs
@@ -0,0 +1,18 @@
+﻿public class WidgetNotImplentedException : System.Exception
+{
+    public WidgetNotImplentedException()
+    {
+    }
+    public WidgetNotImplentedException(string message)
+        : base(message)
+    {
+    }
+    public WidgetNotImplentedException(string message, System.Exception inner)
+        : base(message, inner)
+    {
+    }
+    public string Display()
+    {
+        return ExceptionUtility.Display(this, "WidgetNotImplentedException");
+    }
+}
