26f34aaf1905f36293b7dd6191b4c8c4b389cf5d
steven-p-sutton <steven.p.sutton@gmail.com>
2020-11-26 11:15:29 +0000

    Added WIDGET-Try project to try out model & objects etc (useful anyway longer term)

---
+++ WIDGET-Try/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Conductus.Widget.Object;
+
+namespace Conductus.Widget.Try
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello WIDGET-Try World!");
+            WidgetObject w = new WidgetObject();
+            WidgetDTO wd = new WidgetDTO();
+        }
+    }
+}
