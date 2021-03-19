3406478fe4d83d13a5b3f64305310b1a30299731
steven-p-sutton <steven.p.sutton@gmail.com>
2021-03-19 22:11:07 +0000

    Widget methods virtual

--- WIDGET-Model/clsWidget.cs
+++ WIDGET-Model/clsWidget.cs
@@ -26,7 +26,7 @@ public class WidgetObject : IWidget
     public string Name { get; set; }
     public int Count { get; set; }
     public string Secret { get; set; } // not included in DTO version of class.
-    public string Display(string title)
+    public virtual string Display(string title)
     {
         string s = Environment.NewLine;
         s = s + Heading.H4 + " " + title + " " + Heading.H4 + Environment.NewLine;
@@ -38,7 +38,7 @@ public class WidgetObject : IWidget
         s = s + Environment.NewLine;
         return s;
     }
-    public int Ping (int x, int y)
+    public virtual int Ping (int x, int y)
     {
         return x + y;
     }
