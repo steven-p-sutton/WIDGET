7c3edde08d0cf6e2cd865c22ad3e531018675fbc
steven-p-sutton <steven.p.sutton@gmail.com>
2021-03-29 18:02:45 +0100

    update

---
+++ CODERUN-MyRunner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Conductus.CODERUN.Library.Core;
+using Conductus.CODERUN.WIDGET;
+
+namespace CODERUN_MyRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wd = new WidgetCodeRun();
+            CodeRunFramework.Run(wd);
+        }
+    }
+}
---
+++ CODERUN-Widget/WidgetCodeRun.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Conductus.CODERUN.Library.Core;
+using Conductus.MOCK.Framework.Core;
+using Conductus.WIDGET.Model.Core;
+
+namespace Conductus.CODERUN.WIDGET
+{
+    public class WidgetCodeRun : ICodeRunModule
+    {
+        public string Title
+        {
+            get { return "Widget MOQ Code Run"; }
+        }
+
+        public void Run()
+        {
+            try
+            {
+                var widget = new MWidget
+                {
+                    Run = IMock.RunType.SUCCESS,
+                    Arrange = IMock.RunType.SUCCESS,
+                    Test = IMock.RunType.SUCCESS,
+                    Assert = IMock.RunType.SUCCESS
+                };
+
+                var widget2 = new MWidget
+                {
+                    Run = IMock.RunType.EXCEPTION,
+                    ExceptionExpected = new Exception("Widget Error"),
+                    Throws = IMock.RunType.EXCEPTION,
+                    Arrange = IMock.RunType.EXCEPTION,
+                    Test = IMock.RunType.EXCEPTION,
+                    Assert = IMock.RunType.EXCEPTION
+                };
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
